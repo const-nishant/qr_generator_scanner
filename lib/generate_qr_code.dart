382e88d13a5c24f5e09fb09986dd53f2b860cca3
nishantpatil04 <nishantspatil04@gmail.com>
2024-01-27 18:32:47 +0530

    generateqr_error solved

--- lib/generate_qr_code.dart
+++ lib/generate_qr_code.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
-
-
 class generate_QrCode extends StatefulWidget {
   const generate_QrCode({super.key});
 
@@ -11,39 +9,50 @@ class generate_QrCode extends StatefulWidget {
 }
 
 class _generate_QrCodeState extends State<generate_QrCode> {
-  
-  TextEditingController urlcontroller =TextEditingController();
+  TextEditingController urlcontroller = TextEditingController();
+
+  String qrData = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Generate QR Code'),
-        
       ),
-      body:Center(
+      body: Center(
         child: SingleChildScrollView(
-          child:Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if(urlcontroller.text.isNotEmpty)
-              
-             //QrImage(data: urlcontroller.text,size:200,),//fix this
-              const SizedBox(height: 18,),
-              Container(
-                padding: const EdgeInsets.only(left: 10,right: 10,),
-                child: TextField(
-                  controller: urlcontroller,
-                  decoration: InputDecoration(
-                    hintText: 'Enter your data',
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
-                    labelText: 'Enter your data',
-                  ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (urlcontroller.text.isNotEmpty)
+              QrImageView(
+                data: qrData,
+                size: 200,
+              ), //fix this
+            const SizedBox(
+              height: 18,
+            ),
+            Container(
+              padding: const EdgeInsets.only(
+                left: 10,
+                right: 10,
+              ),
+              child: TextField(
+                controller: urlcontroller,
+                decoration: InputDecoration(
+                  hintText: 'Enter your data',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  labelText: 'Enter your data',
                 ),
               ),
-              const SizedBox(height: 10,),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+             const SizedBox(height: 10,),
               ElevatedButton(onPressed: (){
                 setState(() {
-                  
+                  qrData =urlcontroller.text;
                 });
               }, child: Text("Generate QR Code"),
               
